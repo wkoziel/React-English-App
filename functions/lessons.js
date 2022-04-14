87c59ec2fb713fd1aa75a00ff01bede168fdaa77
Wojciech Kozieł <wojtek98koziel@gmail.com>
2022-04-14 14:04:12 +0200

    Update lessons.js

--- functions/lessons.js
+++ functions/lessons.js
@@ -1,7 +1,6 @@
 exports.handler = async (event, context) => {
-   const lessons = [{ id: 0 }, { id: 1 }];
    return {
       statusCode: 200,
-      body: JSON.stringify(lessons),
+      body: 'Hello world',
    };
 };
